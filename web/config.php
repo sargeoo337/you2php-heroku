1c3f5bcae9bf64f54401e65194a82a94a056d8fe
sargeoo337 <36659595+sargeoo337@users.noreply.github.com>
2018-02-20 18:37:38 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,6 +1,6 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyAAHeeUGBCE0TF7YdAkTyF16ySLM7RboiY');//这里改成您的api key
+define('APIKEY', 'AIzaSyDs-DpDXpPxtIoGzejL-XyntPq2vcLi2lQ');//这里改成您的api key
 define('GJ_CODE', 'HK');//国家代码
 define('SITE_NAME', 'Youtube');//网站名字
 define('TITLENAME', 'Youtube');//网站名字
