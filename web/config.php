70db615dba6b20245531b43e2c7d830f4b0b8ab1
sargeoo337 <36659595+sargeoo337@users.noreply.github.com>
2018-02-20 18:42:20 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -4,6 +4,6 @@ define('APIKEY', 'AIzaSyDs-DpDXpPxtIoGzejL-XyntPq2vcLi2lQ');//这里改成您的
 define('GJ_CODE', 'HK');//国家代码
 define('SITE_NAME', 'Youtube');//网站名字
 define('TITLENAME', 'Youtube');//网站名字
-define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
+define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh5');//一个随机字符串，加解密KEY
 define('EMAIL', 'sargeoo337@gmail.com');//邮箱
 ?>
